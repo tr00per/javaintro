263bdd5a4f484e3aec8ded18492d060ab3fb10c8
Artur Czajka <arturczajka@gmail.com>
2016-12-20 20:15:37 +0100

    Better word count

---
+++ src/main/java/sda/code/intermediate/part3/answers/book/UserDir.java
@@ -0,0 +1,13 @@
+package sda.code.intermediate.part3.answers.book;
+
+import java.io.File;
+import java.nio.file.Paths;
+
+public class UserDir {
+
+	public static void main(String[] args) {
+		System.out.println(System.getProperty("user.home"));
+		File source = Paths.get(System.getProperty("user.home"), "Desktop", "Shakespeare.txt").toFile();
+		System.out.println(source.getAbsolutePath());
+	}
+}
--- src/main/java/sda/code/intermediate/part3/answers/book/WordCount.java
+++ src/main/java/sda/code/intermediate/part3/answers/book/WordCount.java
@@ -2,6 +2,8 @@ package sda.code.intermediate.part3.answers.book;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -9,22 +11,31 @@ import java.util.Scanner;
 public class WordCount {
 
 	public static void main(String[] args) throws IOException {
-		File book = new File("/home/czajka/data/Shakespeare.txt");
-		Scanner sc = new Scanner(book);
-		sc.useDelimiter("\\W+");
+		// File book = new File("/home/czajka/data/Shakespeare.txt");
+		File book = Paths.get(System.getProperty("user.home"), "data", "Shakespeare.txt").toFile();
 
 		Map<String, Integer> words = new HashMap<>();
 
+		try (Scanner sc = new Scanner(book)) {
+			sc.useDelimiter("\\W+");
+
+			gatherStats(sc, words);
+		}
+
+		words.entrySet().stream().sorted((e1, e2) -> e2.getValue() - e1.getValue()).limit(20)
+				.forEach(e -> System.out.println(e.getKey() + " = " + e.getValue()));
+
+		words.entrySet().stream().sorted(Map.Entry.comparingByValue(Collections.reverseOrder())).limit(20)
+				.forEach(e -> System.out.println(e.getKey() + " = " + e.getValue()));
+	}
+
+	private static void gatherStats(Scanner sc, Map<String, Integer> words) {
 		while (sc.hasNext()) {
 			String token = sc.next().toLowerCase();
 			Integer count = words.getOrDefault(token, 0);
 			count += 1;
 			words.put(token, count);
 		}
-
-		sc.close();
-		words.entrySet().stream().sorted((e1, e2) -> e2.getValue() - e1.getValue())
-				.forEach(e -> System.out.println(e.getKey() + " = " + e.getValue()));
 	}
 
 }
