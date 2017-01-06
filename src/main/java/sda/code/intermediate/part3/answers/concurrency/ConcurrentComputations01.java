9aa8b5d9ddcabdd44d83ae9f09289f4c15890448
Artur Czajka <arturczajka@gmail.com>
2017-01-06 15:30:05 +0100

    New exercise for concurrent computations

---
+++ src/main/java/sda/code/intermediate/part3/answers/concurrency/ConcurrentComputations01.java
@@ -0,0 +1,50 @@
+package sda.code.intermediate.part3.answers.concurrency;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import sda.code.intermediate.part1.answers.Sorting;
+import sda.code.intermediate.part1.answers.Algorithms;
+
+public class ConcurrentComputations01 {
+
+	private static class Computation implements Callable<Long> {
+		private int inArr[] = new Algorithms().createRandomArray(10000);
+		private int outArr[];
+
+		@Override
+		public Long call() {
+			System.out.println("Pierwszy element: " + inArr[0]);
+			long start = System.nanoTime();
+			outArr = new Sorting().bubbleSort(inArr);
+			long stop = System.nanoTime();
+			System.out.println("Pierwszy element: " + outArr[0]);
+			return stop - start;
+		}
+
+	}
+
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
+		Computation c1 = new Computation();
+		Computation c2 = new Computation();
+		Computation c3 = new Computation();
+
+		ExecutorService es = Executors.newFixedThreadPool(2);
+
+		long start = System.nanoTime();
+		Future<Long> f1 = es.submit(c1);
+		Future<Long> f2 = es.submit(c2);
+		es.shutdown();
+		long t3 = c3.call();
+		long t1 = f1.get();
+		long t2 = f2.get();
+		long stop = System.nanoTime();
+
+		System.out.println("Suma czasów: " + (t1 + t2 + t3) / 1000000L);
+		System.out.println("Całkowity czas: " + (stop - start) / 1000000L);
+	}
+
+}
---
+++ src/main/java/sda/code/intermediate/part3/exercises/concurrency/ConcurrentComputations.java
@@ -0,0 +1,42 @@
+package sda.code.intermediate.part3.exercises.concurrency;
+
+import sda.code.intermediate.part1.answers.Algorithms;
+import sda.code.intermediate.part1.answers.Sorting;
+
+public class ConcurrentComputations {
+
+	private static class Computation {
+		private int inArr[] = new Algorithms().createRandomArray(10000);
+		private int outArr[];
+
+		public long compute() {
+			System.out.println("Pierwszy element: " + inArr[0]);
+			long start = System.nanoTime();
+			outArr = new Sorting().bubbleSort(inArr);
+			long stop = System.nanoTime();
+			System.out.println("Pierwszy element: " + outArr[0]);
+			return stop - start;
+		}
+	}
+
+	/**
+	 * Zadanie: Przerobić program tak, by długo działające zadania wykonały się
+	 * współbieżnie. Algorytm sortowania powinien pozostać bez zmian. Efektem
+	 * będzie, że suma czasów będzie większa od całkowitego czasu.
+	 */
+	public static void main(String[] args) {
+		Computation c1 = new Computation();
+		Computation c2 = new Computation();
+		Computation c3 = new Computation();
+
+		long start = System.nanoTime();
+		long t1 = c1.compute();
+		long t2 = c2.compute();
+		long t3 = c3.compute();
+		long stop = System.nanoTime();
+
+		System.out.println("Suma czasów: " + (t1 + t2 + t3) / 1000000L);
+		System.out.println("Całkowity czas: " + (stop - start) / 1000000L);
+	}
+
+}
