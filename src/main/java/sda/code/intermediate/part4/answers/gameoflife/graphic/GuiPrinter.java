fa8f1f40ec328ef99d55eea946d76031bb5f4f64
Artur Czajka <arturczajka@gmail.com>
2017-07-11 18:12:07 +0200

    Update for IDEA

---
+++ src/main/java/sda/code/intermediate/part4/answers/gameoflife/graphic/GuiPrinter.java
@@ -0,0 +1,20 @@
+package sda.code.intermediate.part4.answers.gameoflife.graphic;
+
+import sda.code.intermediate.part4.answers.gameoflife.GameEntity;
+import sda.code.intermediate.part4.answers.gameoflife.Printer;
+
+public class GuiPrinter implements Printer {
+
+	@Override
+	public void reset() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void accept(GameEntity e) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
